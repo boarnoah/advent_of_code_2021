5ffc8fd1b8b01bd8b0542c27f27354adcb8127e2
Adipa Wijayathilaka <adipamihi@gmail.com>
2021-12-01 19:52:30 -0500

    Parse file as array of uints, count # increases

--- src/day_one/mod.rs
+++ src/day_one/mod.rs
@@ -1,3 +1,24 @@
-pub fn execute() -> i32 {
-    return 0;
+// https://adventofcode.com/2021/day/1
+pub fn execute() -> u32 {
+    let input = include_str!("input.txt");
+
+    let depths = input.lines().map(|i| i.parse::<u32>());
+    let mut depth_increases = 0;
+    let mut previous_depth = 0;
+
+    for (i, parsed_depth) in depths.enumerate() {
+        match parsed_depth {
+            Ok(depth) => {
+                if previous_depth == 0 {
+                    depth_increases += 1;
+                } else if depth > previous_depth {
+                    depth_increases += 1;
+                }
+                previous_depth = depth;
+            }
+            Err(_) => println!("Couldn't parse depth #{0}", i)
+        }
+    }
+
+    return depth_increases;
 }
