56171caca2947733cf26df9869664f32ce7a66c6
Adipa Wijayathilaka <adipamihi@gmail.com>
2021-12-01 21:14:47 -0500

    Refactored to support multiple solutions in day's problem. Collected depths to vector, consider negative depths to be invalid measurements.

--- src/day_one/mod.rs
+++ src/day_one/mod.rs
@@ -1,24 +1,31 @@
 // https://adventofcode.com/2021/day/1
-pub fn execute() -> u32 {
+pub fn execute() {
     let input = include_str!("input.txt");
 
-    let depths = input.lines().map(|i| i.parse::<u32>());
-    let mut depth_increases = 0;
-    let mut previous_depth = 0;
+    let depths: Vec<i32> = input.lines().map(|i| i.parse::<i32>().unwrap_or(-1)).collect();
+    println!("Number of depth increases: {0}", depth_increases(&depths));
+    println!("Number of depth increases (sampling with sliding window (of 3)): {0}", depth_increases_window_sampling(&depths, 3));
+}
 
-    for (i, parsed_depth) in depths.enumerate() {
-        match parsed_depth {
-            Ok(depth) => {
-                if previous_depth == 0 {
-                    println!("Skipping #{0}, Previous depth unknown.", i)
-                } else if depth > previous_depth {
-                    depth_increases += 1;
-                }
-                previous_depth = depth;
-            }
-            Err(_) => println!("Couldn't parse depth #{0}", i)
+pub fn depth_increases(depths: &[i32]) -> u32 {
+    let mut depth_increases = 0;
+    let mut previous_depth = &depths[0];
+
+    for depth in depths.iter().skip(1) {
+        if *depth < 0 {
+            continue;
         }
+
+        if *depth > *previous_depth {
+            depth_increases += 1;
+        }
+
+        previous_depth = depth;
     }
 
     return depth_increases;
+}
+
+pub fn depth_increases_window_sampling(depths: &[i32], sampling_window: u32) -> u32 {
+    return 0;
 }
