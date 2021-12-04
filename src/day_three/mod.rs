17ffe2b6d1ddea29571e00dd61ce52621c5d4527
Adipa Wijayathilaka <adipamihi@gmail.com>
2021-12-03 21:06:34 -0500

    Refactor count # of 1 bits in each position to function, to re-use for Day 3 Solution Part 2

--- src/day_three/mod.rs
+++ src/day_three/mod.rs
@@ -18,28 +18,17 @@ pub fn execute() {
         .collect();
 
     println!(
-        "Power consumption: {0}",
+        "Power consumption: {}",
         power_consumption(&sensor_readings, num_bits)
+    );
+    println!(
+        "Life Support Rating: {}",
+        life_support_rating(&sensor_readings, num_bits)
     );
 }
 
 fn power_consumption(sensor_readings: &[u16], num_bits: usize) -> u32 {
-    let mut bit_counts: Vec<u32> = vec![0; num_bits];
-
-    for sensor_reading in sensor_readings {
-        // ex: Pretend u8 for simplicity, For i = 3
-        // sensor_reading: 1111 1100
-        // mask (1 << 3):  0000 0001 -> 0000 1000
-        // AND mask with sensor_reading: 0000 1000, if 3rd bit of reading was 0: 0000 0000
-
-        for i in 0..num_bits {
-            let mask = 1 << i;
-
-            if sensor_reading & mask != 0 {
-                bit_counts[i] += 1;
-            }
-        }
-    }
+    let bit_counts: Vec<u32> = count_flagged_bits_by_position(sensor_readings, num_bits);
 
     let mut gamma_rate: u32 = 0;
     let mut epsilon_rate: u32 = 0;
@@ -48,7 +37,7 @@ fn power_consumption(sensor_readings: &[u16], num_bits: usize) -> u32 {
         let mask = 1 << i;
 
         // If this bit is 1 for more than half the readings, 1 is most significant
-        if bit_counts[i] > (sensor_readings.len() / 2) as u32 {
+        if bit_counts[i] > sensor_readings.len() as u32 - bit_counts[i] {
             gamma_rate |= mask
         } else {
             epsilon_rate |= mask
@@ -56,4 +45,36 @@ fn power_consumption(sensor_readings: &[u16], num_bits: usize) -> u32 {
     }
 
     return gamma_rate * epsilon_rate;
+}
+
+fn life_support_rating(sensor_readings: &[u16], num_bits: usize) -> u32 {
+    return 0;
+}
+
+fn count_flagged_bits_in_position(sensor_readings: &[u16], position: usize) -> u32 {
+    let mut bit_count: u32 = 0;
+
+    // ex: Pretend u8 for simplicity, For i = 3
+    // sensor_reading: 1111 1100
+    // mask (1 << 3):  0000 0001 -> 0000 1000
+    // AND mask with sensor_reading: 0000 1000, if 3rd bit of reading was 0: 0000 0000
+    for sensor_reading in sensor_readings {
+        let mask = 1 << position;
+
+        if sensor_reading & mask != 0 {
+            bit_count += 1;
+        }
+    }
+
+    return bit_count;
+}
+
+fn count_flagged_bits_by_position(sensor_readings: &[u16], num_bits: usize) -> Vec<u32> {
+    let mut bit_counts: Vec<u32> = vec![0; num_bits];
+
+    for i in 0..num_bits {
+        bit_counts[i] = count_flagged_bits_in_position(sensor_readings, i)
+    }
+
+    return bit_counts;
 }
