0fe51217a654b13376e557036f5250c7ddc6a2aa
Adipa Wijayathilaka <adipamihi@gmail.com>
2021-12-19 22:48:41 -0500

    Init Day 4 challenge

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod day_four;
 mod day_one;
 mod day_three;
 mod day_two;
@@ -9,4 +10,6 @@ fn main() {
     day_two::execute();
     println!("Day Three:");
     day_three::execute();
+    println!("Day Four:");
+    day_four::execute();
 }
